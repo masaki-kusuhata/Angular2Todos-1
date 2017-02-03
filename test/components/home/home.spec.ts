61085c2cbc1438f096c538782308081d8e6da4b8
Masaki Kusuhata <kusuhata.masaki@jp.fujitsu.com>
2017-02-03 13:07:28 +0900

    add unit test

---
+++ test/components/home/home.spec.ts
@@ -0,0 +1,11 @@
+import { AppComponent } from '../../../src/components/home/home'
+
+import { TestBed } from '@angular/core/testing';
+
+describe('HomeComponent Test', () => {
+   beforeEach(() => {
+       TestBed.configureTestingModule({
+
+       });
+   }) ;
+});
